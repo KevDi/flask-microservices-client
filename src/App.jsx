86f74bbef7b01da5b4882a00975f3b31eabca628
KevDi <kevin@mjkd.de>
2017-07-20 11:20:09 +0200

    Navbar and Form for login/register added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
-import { Route, Switch } from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import axios from 'axios';
 
+import NavBar from './components/Navbar';
+import Form from './components/Form';
 import UsersList from './components/UsersList';
 import AddUser from './components/AddUser';
 import About from './components/About';
@@ -12,7 +14,13 @@ class App extends Component {
         this.state = {
             users: [],
             username: '',
-            email: ''
+            email: '',
+            title: 'TestDriven.io',
+            formData: {
+                username: '',
+                email: '',
+                password: ''
+            }
         }
     }
 
@@ -55,28 +63,43 @@ class App extends Component {
 
     render() {
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-6">
-                        <br/>
-                        <Switch>
-                            <Route exact path='/' render={() => (
-                                <div>
-                                    <h1>All Users</h1>
-                                    <hr/>
-                                    <br/>
-                                    <AddUser
-                                        username={this.state.username}
-                                        email={this.state.email}
-                                        handleChange={ this.handleChange.bind(this) }
-                                        addUser={ this.addUser.bind(this) }
+            <div>
+                <NavBar title={this.state.title}/>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <br/>
+                            <Switch>
+                                <Route exact path='/' render={() => (
+                                    <div>
+                                        <h1>All Users</h1>
+                                        <hr/>
+                                        <br/>
+                                        <AddUser
+                                            username={this.state.username}
+                                            email={this.state.email}
+                                            handleChange={ this.handleChange.bind(this) }
+                                            addUser={ this.addUser.bind(this) }
+                                        />
+                                        <br/>
+                                        <UsersList users={ this.state.users }/>
+                                    </div>
+                                )}/>
+                                <Route exact path='/about' component={About}/>
+                                <Route exact path='/register' render={() => (
+                                    <Form
+                                        formType={'Register'}
+                                        formData={this.state.formData}
                                     />
-                                    <br/>
-                                    <UsersList users={ this.state.users }/>
-                                </div>
-                            )} />
-                            <Route exact path='/about' component={About}/>
-                        </Switch>
+                                )}/>
+                                <Route exact path='/login' render={() => (
+                                    <Form
+                                        formType={'Login'}
+                                        formData={this.state.formData}
+                                    />
+                                )}/>
+                            </Switch>
+                        </div>
                     </div>
                 </div>
             </div>
